82fadbb845fd99a0663669c34175b4a02a6a750d
mokarakaya <mozgkarakaya@gmail.com>
2015-04-16 00:39:10 +0300

    add popularity test

---
+++ src/test/java/scr/PopularityTest.java
@@ -0,0 +1,51 @@
+package scr;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
+import org.apache.mahout.cf.taste.impl.recommender.svd.SVDRecommender;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+
+import scr.ratingManipulation.AverageRatingRecommender;
+
+
+/**
+ * Unit test for AverageRatingRecommender.
+ */
+public class PopularityTest 
+    extends AbstractTest
+{
+	/**
+	 * keeps average rating of items
+	 */
+	private static Map<Long, Float> map = null;
+
+	@Override
+	public Recommender getRecommender(SVDRecommender recommender,
+			double threshold) throws TasteException {
+		if(map==null){
+			map=getPopularityMap(recommender.getDataModel());
+		}
+		//add 4 to threshold since we need to run this test between 4 and 5
+		return new AverageRatingRecommender(recommender, threshold+4, map);
+	}
+	
+	/**
+	 * return average rating of items
+	 * @param dataModel
+	 * @return
+	 * @throws TasteException
+	 */
+	private Map<Long, Float> getPopularityMap(DataModel dataModel) throws TasteException {
+		Map<Long, Float> avgRatingMap=new HashMap<Long, Float>();
+		LongPrimitiveIterator itemIDs = dataModel.getItemIDs();
+		while(itemIDs.hasNext()){
+			Long next = itemIDs.next();
+			avgRatingMap.put(next, (float) dataModel.getPreferencesForItem(next).length());
+		}
+		return avgRatingMap;
+	}
+}
