6a81dffb31b04067a48dac066c9cee537533d5ea
mokarakaya <mozgkarakaya@gmail.com>
2017-02-05 22:53:49 +0300

    edit to run each dataset in a new thread.

--- src/test/java/scr/runner/SVDRunner.java
+++ src/test/java/scr/runner/SVDRunner.java
@@ -13,6 +13,9 @@ import scr.baseRecommender.generic.SVDBaseRecommender;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by mokarakaya on 12.07.2015.
@@ -21,12 +24,32 @@ public class SVDRunner extends TestCase implements  Runner{
 
     public void testApp() throws InterruptedException, TasteException, IOException {
 
+        List<String> datasets=new ArrayList<>();
+        datasets.add("Movielens100K");
+        /*datasets.add("Movielens");
+        datasets.add("Yahoo Music");
+        datasets.add("Bookcrossing");*/
+        ExecutorService executor = Executors.newFixedThreadPool(8);
+        for(String dataset: datasets){
+            Runnable worker= () -> {
+                try {
+                    runTests(dataset);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (TasteException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            };
+            executor.execute(worker);
+        }
 
-
-        runTests("Movielens");
-        //runTests("Yahoo Music");
-        runTests("Bookcrossing");
-        //runTests("Movielens100K");
+        executor.shutdown();
+        if (!executor.awaitTermination(10L, TimeUnit.HOURS)) {
+            System.err.println("Threads didn't finish in 10 hours!");
+            System.err.println("Number of rejected tasks:"+ executor.shutdownNow().size());
+        }
 
     }
 
@@ -37,8 +60,8 @@ public class SVDRunner extends TestCase implements  Runner{
         tests.add(new AggregateSVDGraphTest(baseRecommender,data,1));
         tests.add(new AggregateGraphTest(baseRecommender,data,1));
         //tests.add(new RMTest(baseRecommender));
-        //tests.add(new PopularityTest(baseRecommender,1));
-        //tests.add(new AverageRatingTest(baseRecommender,1));
+        tests.add(new PopularityTest(baseRecommender,1));
+        tests.add(new AverageRatingTest(baseRecommender,1));
         test.testApp("SVD "+data, tests);
     }
 }
