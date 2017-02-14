b689d073091d9e0d8880e5f3991ebb3f290f4324
mokarakaya <mozgkarakaya@gmail.com>
2017-02-14 04:29:52 +0300

    add custom-stylesheet

--- src/main/java/scr/draw/XYChartCreator.java
+++ src/main/java/scr/draw/XYChartCreator.java
@@ -59,7 +59,7 @@ public class XYChartCreator extends Application implements Runnable {
             lineChart.getData().add(series);
         }
         Scene scene  = new Scene(lineChart,800, 600);
-        scene.getStylesheets().add("stylesheet.css");
+        scene.getStylesheets().add(XYChartCreator.class.getResource("/custom-stylesheet.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
         WritableImage wim = new WritableImage((int) stage.getWidth(),  (int) stage.getHeight());
